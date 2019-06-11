c1309249200f473823c9e64b55e968dab556a729
elisek-bc <elisekppns@gmail.com>
2019-06-11 14:30:34 +0200

    readme added

--- app/controllers/note.controller.js
+++ app/controllers/note.controller.js
@@ -8,21 +8,21 @@ exports.create = (req, res) => {
         });
     };
 
-// Create a Note
-const note = new Note({
-    title: req.body.title || "Untitled Note", 
-    content: req.body.content
-});
-
-// Save Note in the database
-note.save()
-.then(data => {
-    res.send(data);
-}).catch(err => {
-    res.status(500).send({
-        message: err.message || "Some error occurred while creating the Note."
+    // Create a Note
+    const note = new Note({
+        title: req.body.title || "Untitled Note", 
+        content: req.body.content
     });
-});
+
+    // Save Note in the database
+    note.save()
+    .then(data => {
+        res.send(data);
+    }).catch(err => {
+        res.status(500).send({
+            message: err.message || "Some error occurred while creating the Note."
+        });
+    });
 };
 
 // Retrieve and return all notes from the database.
---
+++ readme.md
@@ -0,0 +1,19 @@
+# First CRUD API
+
+## What?
+This is an execution of a tutorial found [here](https://www.callicoder.com/node-js-express-mongodb-restful-crud-api-tutorial/) on making a Restful CRUD API with Node.js, Express and MongoDB.
+
+## Contributors
+I, Elise, am the only contributor to this project.
+
+## Timing
+This exercise was done as an introduction to NodeJS and making your own API's. There's not really a deadline on it. It's part of my educational training at Becode. I worked on it on the 11th June 2019.
+
+## How?
+* MongoDB
+* Express
+* NodeJS
+* Javascript
+
+## Struggles
+Getting things done in Postman seems difficult. I don't managed to make this work yet.
